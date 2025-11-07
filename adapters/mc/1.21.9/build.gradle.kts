9523283eb17e919766daa44692a3dbdaab220594
Dainsleif <dainsleif@yeah.net>
2025-11-08 00:04:31 +0800

    feat(version): 支持 1.21.9+
    - 支持 1.21.9+
    - loom 更新到 1.11，并在 gradle.properties 中统一管理 loom 版本
    - fabric-loader 版本不再影响依赖
    - 引入 bug：无法显示皮肤（请求不到 /player/certificates）

--- adapters/mc/1.21.9/build.gradle.kts
+++ adapters/mc/1.21.9/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 }
 
 adapter {
-    minecraft = "1.20"
+    minecraft = "1.21.9"
     yarn = 1
-    loader = "0.16.10"
-    api = "0.83.0"
-    authlib = "4.0.43"
+    loader = "0.17.3"
+    api = "0.134.0"
+    authlib = "7.0.61"
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,7 +6,3 @@ repositories {
     gradlePluginPortal()
     maven(url = "https://maven.fabricmc.net/")
 }
-
-dependencies {
-    compileOnly("fabric-loom:fabric-loom.gradle.plugin:1.10-SNAPSHOT")
-}
