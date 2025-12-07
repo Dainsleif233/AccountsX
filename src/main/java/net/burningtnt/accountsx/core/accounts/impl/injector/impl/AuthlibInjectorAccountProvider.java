079a36adf64e8a9c3a21e717a3d202be6f897821
Dainsleif <dainsleif@yeah.net>
2025-12-07 14:39:24 +0800

    feat(ali): 支持 authlib-injector API 地址指示
    - 更新语言文件
    - 重构AuthlibInjectorAccountProvider的URL处理
    - 在NetworkUtils中添加headRequest方法用于处理HTTP HEAD请求
    - 调整AbstractInjectorAccountProvider的URL拼接方式

--- src/main/java/net/burningtnt/accountsx/core/accounts/impl/injector/impl/AuthlibInjectorAccountProvider.java
+++ src/main/java/net/burningtnt/accountsx/core/accounts/impl/injector/impl/AuthlibInjectorAccountProvider.java
@@ -3,7 +3,12 @@ package net.burningtnt.accountsx.core.accounts.impl.injector.impl;
 import net.burningtnt.accountsx.core.accounts.impl.injector.AbstractInjectorAccount;
 import net.burningtnt.accountsx.core.accounts.impl.injector.AbstractInjectorAccountProvider;
 import net.burningtnt.accountsx.core.accounts.model.AccountType;
+import net.burningtnt.accountsx.core.utils.NetworkUtils;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public final class AuthlibInjectorAccountProvider extends AbstractInjectorAccountProvider<AuthlibInjectorAccountProvider.AuthlibInjectorAccount> {
@@ -13,7 +18,34 @@ public final class AuthlibInjectorAccountProvider extends AbstractInjectorAccoun
 
     @Override
     protected String transformServerBaseURL(String server) {
-        return "https://" + server + "/api/yggdrasil/";
+        String api = buildUrl(server);
+        int redirects = 0;
+        while (true) {
+            try {
+                Map<String, List<String>> headers = NetworkUtils.headRequest(api);
+                if (!headers.containsKey("X-Authlib-Injector-API-Location"))
+                    if (redirects == 0) return "https://" + server + "/api/yggdrasil/";
+                    else return api;
+                String newApi = headers.get("X-Authlib-Injector-API-Location").get(0);
+                if (!newApi.equals(api)) {
+                    redirects++;
+                    if (redirects > 10) {
+                        throw new IOException("Too many redirects");
+                    }
+                    api = newApi;
+                    continue;
+                }
+                return api;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private String buildUrl(String server) {
+        URI u = URI.create(server);
+        if (u.getScheme() != null) return server;
+        return "http://" + server;
     }
 
     @Override
