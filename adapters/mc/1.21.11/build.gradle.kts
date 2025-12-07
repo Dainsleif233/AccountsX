54a8936cd492a8470aa9c4c5b2886ffbc0999852
Dainsleif <dainsleif@yeah.net>
2025-12-07 19:21:15 +0800

    ver(1.21.11): 预支持1.21.11
    - 支持1.21.11-rc2
    - 拼接api版本排除后缀
    - loom更新到1.13

---
+++ adapters/mc/1.21.11/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("fabric-loom")
+    id("accountsx.mc.adapter")
+}
+
+adapter {
+    minecraft = "1.21.11-rc2"
+    yarn = 1
+    loader = "0.18.1"
+    api = "0.139.4"
+    authlib = "7.0.61"
+}
---
+++ adapters/mc/1.21.11/src/main/java/net/burningtnt/accountsx/adapters/mc/mixins/PlayerSkinProviderAccessor.java
@@ -0,0 +1,16 @@
+package net.burningtnt.accountsx.adapters.mc.mixins;
+
+import net.minecraft.client.texture.PlayerSkinTextureDownloader;
+import org.spongepowered.asm.mixin.Unique;
+
+import java.nio.file.Path;
+import java.util.concurrent.Executor;
+
+public interface PlayerSkinProviderAccessor {
+    @Unique
+    Path accountsX$getDirectory();
+
+    PlayerSkinTextureDownloader accountsX$getDownloader();
+
+    Executor accountsX$getExecutor();
+}
