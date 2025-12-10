04dbb978514a97eda0a0803677829c5ba9233b78
Dainsleif <dainsleif@yeah.net>
2025-12-10 11:47:25 +0800

    update(deps): 支持1.21.11

--- adapters/mc/1.21.11/build.gradle.kts
+++ adapters/mc/1.21.11/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 }
 
 adapter {
-    minecraft = "1.21.11-rc2"
+    minecraft = "1.21.11"
     yarn = 1
-    loader = "0.18.1"
+    loader = "0.18.2"
     api = "0.139.4"
     authlib = "7.0.61"
 }
